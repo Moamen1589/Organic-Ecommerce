a5797cf0d9f16a88cbd995182529ce310d9854eb
Moamen Adel <167950921+Moamen1589@users.noreply.github.com>
2025-01-28 13:39:18 +0200

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ function Home() {
         <>
         <NavBar/>
             <SideCart />
-            <div className="Land h-screen m flex items-center max-sm:pt-36  ">
+            <div className="Land h-screen m flex items-center pt-12 max-sm:pt-36  ">
                 <div className='intro-land flex  gap-10 flex-col justify-evenly  basis-4/5 max-lg:basis-full content-center p-7 max-sm:w-full '>
                     <h1 className=' text-8xl  max-md:text-5xl max-md:leading-relaxed leading-normal'><span className='text-green-600 font-bold'>Organic</span> Foods at your<span className=' font-bold'>Doorsteps</span></h1>
                     <p className=' text-gray-500 text-3xl '>Dignissim massa diam elementum.</p>
