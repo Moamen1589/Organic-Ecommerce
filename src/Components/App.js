c21326365771e22462d066f5887f0950ebda1fc3
Moamen Adel <167950921+Moamen1589@users.noreply.github.com>
2025-02-19 23:32:29 +0200

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,30 @@
-import NavBar from './NavBar';
 import Home from './Home';
 import ShowDetails from './ShowDetails';
-import SideCart from './SideCart';
 import Shop from './Shop';
 import SignIn from '../Auth/SignIn';
 import SignUp from '../Auth/SignUp';
 import Profile from '../Auth/Profile';
+import CheckOut from './CheckOut';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import ShopContext from '../Context/ShopContext';
 function App() {
-
-    return (
-        <>
-            <NavBar />
-            <Home />
-            <SideCart />
-            <Shop />
-            <ShowDetails />
-            <SignIn />
-            <SignUp />
-            <Profile />
-        </>
-    );
+  return ( 
+    
+    <Router>
+        <ShopContext>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/details" element={<ShowDetails />} />
+        <Route path="/checkout" element={<CheckOut />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      </ShopContext>
+    </Router>
+  
+  );
 }
 
 export default App;
